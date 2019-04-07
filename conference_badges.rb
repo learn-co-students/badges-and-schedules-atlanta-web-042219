8f04b5a72227bf4384024e65d756d81a8352c80f
han xu <hanxu27@gmail.com>
2019-04-07 03:02:08 +0000

    Automatically backed up by Learn

---
+++ conference_badges.rb
@@ -0,0 +1,17 @@
+# Write your code here.
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}"
+  "Hello, my name is #{name}"
+end
+
+def batch_badget_creator(arr)
+  i = 0
+  
+  arr.each do |name|
+    arr[i] = "Hello, my name is ${name}"
+    i += 1
+  end
+  arr
+end
