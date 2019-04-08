64578cfe5c4a4ee52964d24da18cc98502e1ae98
Andrew Allen <asallen67@gmail.com>
2019-04-07 22:23:47 -0500

    1

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+def badge_maker(name)
+	return "Hello, my name is #{name}."
+
+end
+
+def batch_badge_creator(ppl)
+	arr=[]
+	ppl.each do |x|
+		arr.push("Hello, my name is #{x}.")
+	end
+	return arr
+end
+
+def assign_rooms(ppl)
+	arr=[]
+	room=1
+	ppl.each do |x|
+		arr.push("Hello, #{x}! You'll be assigned to room #{room}!")
+		room+=1
+	end
+	return arr
+end
+
+def printer(ppl)
+	batch_badge_creator(ppl).each do |x|
+		puts x
+	end
+	assign_rooms(ppl).each do |x|
+		puts x
+	end
+end
