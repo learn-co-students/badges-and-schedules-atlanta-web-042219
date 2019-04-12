432b2b38357278231137552ab990c4655e4d2f05
github username <github email address>
2019-04-12 12:58:38 -0400

    Printer is broken

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def badge_maker(name)
+ return "Hello, my name is #{name}."
+end
+
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(speakers)
+  badge_created = []
+  speakers.each do |badges|
+  badge_created.push(badge_maker(badges))
+  end
+  badge_created
+end
+
+def assign_rooms(speakers)
+  room_assigned = []
+  speakers.each_with_index do |room, index|
+  room_assigned.push("Hello, #{room}! You'll be assigned to room #{index + 1}!")
+end
+room_assigned
+end
+
+def printer(speakers)
+  print "#{batch_badge_creator(speakers)}"
+  print "#{assign_rooms(speakers)}"
+  end
